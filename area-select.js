192cabcd40935b8f89d0c89efcc8665ef1d12f4a
Dean Harding <dean@codeka.com.au>
2017-05-02 17:45:40 +1000

    Added a basic area select tool.

--- area-select.js
+++ area-select.js
@@ -13,3 +13,62 @@ function _getQueryVariable(variable) {
 }
 
 $("image").src = _getQueryVariable("filename");
+
+var _top = $("top");
+var _bottom = $("bottom");
+var _left = $("left");
+var _right = $("right");
+var dragging = false;
+var finished = false;
+var rect = {x: 0, y: 0, width: 0, height: 0 };
+
+document.addEventListener("mousedown", function(event) {
+    if (finished) {
+        return;
+    }
+    dragging = true;
+    rect.x = event.pageX;
+    rect.y = event.pageY;
+    _top.style.height = rect.y + "px";
+    _left.style.top = rect.y + "px";
+    _left.style.left = "0px";
+    _left.style.width = rect.x + "px";
+    _right.style.top = rect.y + "px";
+    return false;
+});
+
+document.addEventListener("mousemove", function(event) {
+    if (finished || !dragging) {
+        return;
+    }
+
+    rect.width = event.pageX - rect.x;
+    rect.height = event.pageY - rect.y;
+    // TODO: handle going from bottom-right to top-left as well.
+    _right.style.left = (rect.x + rect.width) + "px";
+    _left.style.height = rect.height + "px";
+    _right.style.height = rect.height + "px";
+    _right.style.right = "0";
+    _bottom.style.top = (rect.y + rect.height) + "px";
+    _bottom.style.bottom = "0";
+});
+
+document.addEventListener("mouseup", function() {
+    if (finished) {
+        return;
+    }
+    dragging = false;
+    _top.style.display = "none";
+    _left.style.display = "none";
+    _right.style.display = "none";
+    _bottom.style.display = "none";
+
+    var canvas = document.createElement("canvas");
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage($("image"), rect.x, rect.y, rect.width, rect.height, 0, 0, rect.width, rect.height);
+    $("image").src = canvas.toDataURL();
+
+    finished = true;
+});
